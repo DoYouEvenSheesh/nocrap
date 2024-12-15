8728eb51ba3e19aad677a44de03a6eb5320f5405
Poseidon <92352131+DoYouEvenSheesh@users.noreply.github.com>
2024-12-15 12:02:36 +0530

    add routes

--- src/app.py
+++ src/app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Hello! This is the main page! <h1>STFU</h1>"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
