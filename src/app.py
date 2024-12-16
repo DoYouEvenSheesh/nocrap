5dbde07d9bfaea15f0fb70dc3c19d7b6d17eae22
Poseidon <92352131+DoYouEvenSheesh@users.noreply.github.com>
2024-12-16 09:45:40 +0530

    add POST route

--- src/app.py
+++ src/app.py
@@ -1,11 +1,18 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+    output = None
+    
+    if request.method == "POST":
+        user_input = request.form.get("userInput", "")
+
+        output = user_input[::-1]
+    return render_template("index.html", output=output)
+
 
 if __name__ == "__main__":
     app.run()
