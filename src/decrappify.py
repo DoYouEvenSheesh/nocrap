7dc68c0e5237ae75d35653cececd8ea3a4f54712
Poseidon <92352131+DoYouEvenSheesh@users.noreply.github.com>
2024-12-18 21:41:32 +0530

    Revert "Added stream"
    This reverts commit 6e58db1390bc52fdea157989a16a505fd09e4ad8.
    
    reverting changes

--- src/decrappify.py
+++ src/decrappify.py
@@ -33,13 +33,10 @@ def grokOutput(input: str):
                 "role": "user",
                 "content" : input,
             },
-        ],
-        stream = True
+        ]
     )
 
-    for event in message:
-        if event.type == "content_block_delta":
-            print(event.delta.text, end = "", flush = True) 
+    return (str(message.content[0].text))
 
 
 
